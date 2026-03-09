8287b36a5c5bf7064859c650ef202e5866967805
Gilguitar88 <136395927+Gilguitar88@users.noreply.github.com>
2026-03-09 18:21:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // ═══════════════════════════════════════════════════════════
 
 const SUPABASE_URL  = 'https://mmenxzajwiaecqzgcher.supabase.co';
-const SUPABASE_ANON = 'sb_publishable_4PWMeWaITqVCvquWNnscNw_iX9eI1b3';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tZW54emFqd2lhZWNxemdjaGVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODQ5ODIsImV4cCI6MjA4ODY2MDk4Mn0.59KABxTspQpozF53StVEsWeVmt8jS9UdwvEI2EBrMJg';
 
 const USER_ID = 'gilmar';
